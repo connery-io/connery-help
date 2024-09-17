892968e9e409a71c633b77aa07a614206e6654ed
Volodymyr Machula <machulav@gmail.com>
2024-09-17 23:29:26 +0200

    Fix tests

--- test/actions/listActions.test.ts
+++ test/actions/listActions.test.ts
@@ -3,11 +3,7 @@ import { handler } from '../../src/actions/listActions';
 // TODO #1: Rename xit() to it() to enable the test.
 // TODO #2: Change the test name below to match your use case.
 xit('should verify if the listActions action works as expected', async () => {
-  const input = {
-    /* TODO #3: Specify input for the action. */
-  };
-
-  const result = await handler({ input });
+  const result = await handler();
 
   expect(result).toEqual({
     /* TODO #4: Specify the expected value of the result. */
