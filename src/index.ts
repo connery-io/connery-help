b1683d81ab773091c6593dabaadde93372d26647
Volodymyr Machula <machulav@gmail.com>
2024-10-29 21:47:08 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import askDocs from './actions/askDocs.js';
 const pluginDefinition: PluginDefinition = {
   name: 'Connery Help',
   description:
-    'Connery Help is a plugin designed to provide easy access to Connery documentation, gather user feedback, report bugs, and list available actions for a streamlined user experience.',
+    'Connery Help is a plugin designed to provide easy access to Connery documentation and allows users to send feedback and report bugs to the Connery team.',
   actions: [sendFeedback, reportBug, askDocs],
 };
 
